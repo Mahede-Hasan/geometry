9dc2c78b0b7789f5126104eef524451fc9ea9304
Mahede Hasan <mahedehasan.cd91@gmail.com>
2024-03-15 15:27:41 +0600

    ellipse

--- scripts/area.js
+++ scripts/area.js
@@ -33,3 +33,32 @@ function calculateRectangleArea() {
     const area = document.getElementById('rectangle-area-value');
     area.innerText = rectangleArea;
 }
+
+// Ellipse
+function calculateEllipseArea(){
+    const majorInput = getInputField('ellipse-large');
+    const minorInput = getInputField('ellipse-short');
+    const area = 3.14 * majorInput * minorInput;
+    setElementText('ellipse-area-value', area);
+}
+
+// reuseable function / reduce code
+
+function calculateParallelogramArea() {
+    const width = getInputField('parallelogram-width');
+    const height = getInputField('parallelogram-height');
+    const area = width * height;
+    setElementText('parallelogram-area-value', area)
+}
+
+function getInputField(inputId) {
+    const inputField = document.getElementById(inputId);
+    const inputFieldText = inputField.value;
+    const inputValue = parseFloat(inputFieldText);
+    return inputValue;
+}
+
+function setElementText(elementId, area) {
+    const elementText = document.getElementById(elementId);
+    elementText.innerText = area;
+}
